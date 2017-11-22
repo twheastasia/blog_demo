e41f32fb6a96780830573fdc78a76079476f7b07
twh <twh@twhdeiMac.local>
2017-11-22 17:41:28 +0200

    add some pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {Input} from 'antd';
-
-const Search = Input.Search;
+import {Input, Button} from 'antd';
+const { TextArea } = Input;
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: false,
+      iconLoading: false,
+    };
+    this.enterLoading = this.enterLoading.bind(this);
+    this.enterIconLoading = this.enterIconLoading.bind(this);
+    this.onContentChange = this.onContentChange.bind(this);
+    this.onTitleChange = this.onTitleChange.bind(this);
+  }
+
+
+  enterLoading = () => {
+    this.setState({ loading: true });
+  }
+
+  enterIconLoading = () => {
+    this.setState({ iconLoading: true });
+  }
+
+  onTitleChange(title){
+    this.setState({title: title.target.value});
+    console.log(this.state);
+  }
+
+  onContentChange(content){
+    this.setState({content: content.target.value});
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,11 +46,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <Search
-          placeholder="input search text"
-          style={{ width: 200 }}
-          onSearch={value => console.log(value)}
-        />
+        <div className="example-input">
+          <h3>文章标题:</h3>
+          <Input size="large" placeholder="标题" onChange={this.onTitleChange}/>
+        </div>
+        <div style={{width:400}}>
+          <h3>文章正文:</h3>
+          <TextArea rows={8} placeholder="正文内容" onChange={this.onContentChange}/>
+        </div>
+        <br/>
+        <Button type="primary" loading={this.state.loading} onClick={this.enterLoading}>
+          提交
+        </Button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Rater from './view/comments'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App />
+    <Rater />
+  </div>, 
+  document.getElementById('root'));
 registerServiceWorker();
